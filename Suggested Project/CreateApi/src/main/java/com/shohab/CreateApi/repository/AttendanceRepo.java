320f45518016e84df3059efff4b4f969cc2e19e7
Tamim Ahmed Raju <138282465+mrahmed17@users.noreply.github.com>
2024-10-02 18:52:31 +0600

    updated

--- Suggested Project/CreateApi/src/main/java/com/shohab/CreateApi/repository/AttendanceRepo.java
+++ Suggested Project/CreateApi/src/main/java/com/shohab/CreateApi/repository/AttendanceRepo.java
@@ -11,6 +11,6 @@ import java.sql.Date;
 public interface AttendanceRepo extends JpaRepository<Attendance,Integer> {
 
 //    @Query("select count(a.employee.id) from Attendance a where a.employee.id = :id and Date(a.checkIn) like %:days%")
-    @Query(value="select count(id) from Attendance where empid = :id and substring(check_in, 6, 2) = :month",nativeQuery = true)
-    int findAttendanceByCount(Long id, String month);
+@Query(value = "select count(id) from Attendance where empid = :id and substring(check_in, 6, 2) = :month", nativeQuery = true)
+int findAttendanceByCount(Long id, String month);
 }
